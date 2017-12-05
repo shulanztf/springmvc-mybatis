364b1232153a29250feb949ebdf9e66ff0866520
1426763507@qq.com <1426763507@qq.com>
2017-12-05 20:32:54 +0800

    springMVC+scala+maven

---
+++ src/main/scala/com/hhcf/backent/Student.scala
@@ -0,0 +1,7 @@
+package com.hhcf.backent
+
+object Student {
+  def main(args:Array[String]){
+    println("aaaaaaaaaaaaaaaa")
+  }
+}
---
+++ src/main/scala/com/hhcf/backent/WordCount.scala
@@ -0,0 +1,14 @@
+package com.hhcf.backent
+/**
+ *
+ */
+class WordCount {
+
+}
+
+object WordCount {
+  def main(args: Array[String]) {
+    println("aaaaaaaaaaaa")
+  }
+
+}
